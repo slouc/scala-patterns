05cb48df5e189b2fe3c1a6f270eaaff4f02126f8
slouc <sinisalouc@gmail.com>
2015-10-25 15:50:36 +0100

    moved comments about printout

--- src/main/scala/initializationorder/InitializationOrder.scala
+++ src/main/scala/initializationorder/InitializationOrder.scala
@@ -10,25 +10,22 @@ class FooWillExtendNum extends { val num = 5 } with FooTrait
 
 object OrderMain extends App {
 
-  // prints 0
   new Foo()
 
-  // same as above, prints 0
   new FooTrait() { val num = 5 }
 
-  // prints 5
   new { val num = 5 } with FooTrait
   
-  // prints 0
   new FooWithoutNum with FooTrait { val num = 5 } 
   
-  // prints 5
   new FooWithNum with FooTrait
   
-  // prints 5
   new FooWillExtendNum
   
   
+  
+  // Prints 0 0 5 0 5 5 
+  //
   // CONCLUSION:
   // If vals are declared before trait is 'mentioned' when creating new instance of class, 
   // they will be instantiated with given values in the trait.
