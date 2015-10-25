47b099c46d2bbca1e35241dcf87d42e948d00362
slouc <sinisalouc@gmail.com>
2015-10-25 15:48:41 +0100

    added case with extending anonymous class

--- src/main/scala/initializationorder/InitializationOrder.scala
+++ src/main/scala/initializationorder/InitializationOrder.scala
@@ -1,28 +1,33 @@
 package initializationorder
 
 
-trait FooTrait { val num: Int; println(num) }
+trait FooTrait { val num: Int; print(num + " ") }
 
 class Foo extends FooTrait { override val num = 5 }
-class FooWithoutFooTrait
-class FooWithoutFooTrait2 { val num = 5 }
+class FooWithoutNum
+class FooWithNum { val num = 5 }
+class FooWillExtendNum extends { val num = 5 } with FooTrait
 
 object OrderMain extends App {
 
   // prints 0
-  val foo = new Foo()
+  new Foo()
 
   // same as above, prints 0
-  val foo2 = new FooTrait() { val num = 5 }
+  new FooTrait() { val num = 5 }
 
   // prints 5
-  val foo3 = new { val num = 5 } with FooTrait
+  new { val num = 5 } with FooTrait
   
   // prints 0
-  val foo4 = new FooWithoutFooTrait with FooTrait { val num = 5 } 
+  new FooWithoutNum with FooTrait { val num = 5 } 
   
   // prints 5
-  val foo5 = new FooWithoutFooTrait2 with FooTrait
+  new FooWithNum with FooTrait
+  
+  // prints 5
+  new FooWillExtendNum
+  
   
   // CONCLUSION:
   // If vals are declared before trait is 'mentioned' when creating new instance of class, 
