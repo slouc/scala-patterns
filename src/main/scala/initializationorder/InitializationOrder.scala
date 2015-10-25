de1e2a3e78c268fccb44cd9a4030a5300fafca48
slouc <sinisalouc@gmail.com>
2015-10-25 20:35:49 +0100

    added class parameter case

--- src/main/scala/initializationorder/InitializationOrder.scala
+++ src/main/scala/initializationorder/InitializationOrder.scala
@@ -4,39 +4,36 @@ package initializationorder
 trait FooTrait { val num: Int; print(num + " ") }
 
 class Foo extends FooTrait { override val num = 5 }
+class FooNumParam(val num: Int)
 class FooWithoutNum
 class FooWithNum { val num = 5 }
-class FooWillExtendNum extends { val num = 5 } with FooTrait
+class FooExtendsNum extends { val num = 5 }
 
 abstract class FooAbstract(val num: Int) { print(num + " ") }
 abstract class FooAbstract2 { val num: Int; print(num + " ") }
 
-class FooExtendsAbstract(override val num: Int) extends FooAbstract(num)
-class FooExtendsAbstract2 extends FooAbstract2 { override val num = 5 }
+class FooAbstractWithNum(override val num: Int) extends FooAbstract(num)
+class FooAbstractWithoutNum extends FooAbstract2 { override val num = 5 }
 
 
 object OrderMain extends App {
 
   new Foo()
-
+  new FooNumParam(5) with FooTrait
+  new FooWithoutNum with FooTrait { val num = 5 } 
+  new FooWithNum with FooTrait
+  new FooExtendsNum with FooTrait
+  
+  println("\n===")
+  
+  new FooAbstractWithNum(5)
+  new FooAbstractWithoutNum()
+  
+  println("\n===")
+  
   new FooTrait() { val num = 5 }
-
   new { val num = 5 } with FooTrait
   
-  new FooWithoutNum with FooTrait { val num = 5 } 
-  
-  new FooWithNum with FooTrait
-  
-  new FooWillExtendNum
-  
-  // same goes for abstract class
-  
-  new FooExtendsAbstract(5)
-  
-  new FooExtendsAbstract2()
-  
-  
-  // Prints 0 0 5 0 5 5 5 0
   //
   // CONCLUSION:
   // If vals are declared before trait is 'mentioned' when creating new instance of class, 
