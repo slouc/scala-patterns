b81ca95d03d889156181d203a5e5d6dcd3e25547
slouc <sinisalouc@gmail.com>
2015-10-25 15:57:52 +0100

    added abstract class examples

--- src/main/scala/initializationorder/InitializationOrder.scala
+++ src/main/scala/initializationorder/InitializationOrder.scala
@@ -8,6 +8,13 @@ class FooWithoutNum
 class FooWithNum { val num = 5 }
 class FooWillExtendNum extends { val num = 5 } with FooTrait
 
+abstract class FooAbstract(val num: Int) { print(num + " ") }
+abstract class FooAbstract2 { val num: Int; print(num + " ") }
+
+class FooExtendsAbstract(override val num: Int) extends FooAbstract(num)
+class FooExtendsAbstract2 extends FooAbstract2 { override val num = 5 }
+
+
 object OrderMain extends App {
 
   new Foo()
@@ -22,9 +29,14 @@ object OrderMain extends App {
   
   new FooWillExtendNum
   
+  // same goes for abstract class
+  
+  new FooExtendsAbstract(5)
+  
+  new FooExtendsAbstract2()
   
   
-  // Prints 0 0 5 0 5 5 
+  // Prints 0 0 5 0 5 5 5 0
   //
   // CONCLUSION:
   // If vals are declared before trait is 'mentioned' when creating new instance of class, 
